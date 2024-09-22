bdfab22491787c90b96151120ac886bd91717c8d
redukee-hse <daartemev@edu.hse.ru>
2024-09-22 16:16:11 +0300

    Вывод разности первого числа из второго

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ ListOfNumbers = [int(x) for x in input().split()]
 num1 = ListOfNumbers[0]
 num2 = ListOfNumbers[1]
 print("Сумма чисел равна", num1 + num2)
+print("Разность чисел", num1, "и", num2, "равна", num1 - num2)
