4ad60746d2dea4099c128b92569cf4de6cbb2cdf
redukee-hse <daartemev@edu.hse.ru>
2024-09-22 16:19:10 +0300

    Вывод частного первого числа от второго

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ print("Hello, Git!")
 print("Введите 2 числа через пробел:")
 ListOfNumbers = [int(x) for x in input().split()]
 print("Произведение чисел равно", ListOfNumbers[0] * ListOfNumbers[1])
+print("Частное числа", ListOfNumbers[0], "от числа", ListOfNumbers[1], "равна", ListOfNumbers[0] / ListOfNumbers[1])
