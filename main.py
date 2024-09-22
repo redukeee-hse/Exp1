cedda0c539312f1cf6fb615e62df8ae5ad3da3dd
redukee-hse <daartemev@edu.hse.ru>
2024-09-22 16:17:38 +0300

    Вывод произведения чисел

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("Hello, Git!")
 print("Введите 2 числа через пробел:")
 ListOfNumbers = [int(x) for x in input().split()]
+print("Произведение чисел равно", ListOfNumbers[0] * ListOfNumbers[1])
