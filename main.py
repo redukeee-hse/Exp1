c0a422984751f6082511f5ebc18d39467924a682
Денис Артемьев <daartemev@edu.hse.ru>
2024-09-22 16:24:07 +0300

    Замена знаков на ошибочные

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ print("Введите 2 числа через пробел:")
 ListOfNumbers = [int(x) for x in input().split()]
 num1 = ListOfNumbers[0]
 num2 = ListOfNumbers[1]
-print("Сумма чисел равна", num1 + num2)
-print("Разность чисел", num1, "и", num2, "равна", num1 - num2)
+print("Сумма чисел равна", num1 - num2)
+print("Разность чисел", num1, "и", num2, "равна", num1 + num2)
+print("Где-то ошибка -_-")
